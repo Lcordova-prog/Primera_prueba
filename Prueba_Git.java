b50fea77872e56e0e67cdbed0d2cb8a07520dbef
Lcordova-prog <lcordova.7915@unimar.edu.ve>
2022-01-29 17:29:16 -0400

    Coloque ahora la libreria Scanner

--- Prueba_Git.java
+++ Prueba_Git.java
@@ -3,18 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.mavenproject1;
+ 
+import java.util.Scanner;
 
-/**
- *
- * @author Pc1
- */
 public class Prueba_Git {
 
+  
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        System.out.println("Introduzca un valor");
+        numero = entrada.nextInt();
+        System.out.println("El valor es:"+numero);
+       
         // TODO code application logic here
     }
     
