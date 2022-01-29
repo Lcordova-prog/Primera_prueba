fcb4a19f3466e2c5e803e27928ded06aae650eae
Lcordova-prog <lcordova.7915@unimar.edu.ve>
2022-01-29 16:16:38 -0400

    Primer commit para practica de GitHub

---
+++ Prueba_Git.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Pc1
+ */
+public class Prueba_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        // TODO code application logic here
+    }
+    
+}
